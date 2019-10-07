84b4dcbb9724bdb9d5ee5661777a31ad15c17eda
healthypackrat <healthypackrat@gmail.com>
2019-10-08 06:33:30 +0900

    Updates

---
+++ precache-manifest.89faaa64ddb6913f190aea0d8c63c5bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3709b6fd37aa7344fb92181f2247a148",
+    "url": "/rails-class-table-v2/index.html"
+  },
+  {
+    "revision": "ca05154ae915dc59678e",
+    "url": "/rails-class-table-v2/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7448470af9085bfe3e01",
+    "url": "/rails-class-table-v2/static/css/main.aad5da48.chunk.css"
+  },
+  {
+    "revision": "ca05154ae915dc59678e",
+    "url": "/rails-class-table-v2/static/js/2.b9bd116e.chunk.js"
+  },
+  {
+    "revision": "7448470af9085bfe3e01",
+    "url": "/rails-class-table-v2/static/js/main.f000b6cc.chunk.js"
+  },
+  {
+    "revision": "57ea7534ac7dc28b930f",
+    "url": "/rails-class-table-v2/static/js/runtime-main.740de0e0.js"
+  }
+]);
