6a5cf432ade11be052a4c4444f7fe1d377541903
healthypackrat <healthypackrat@gmail.com>
2019-10-12 09:11:11 +0900

    Pass count to ResultCount

--- src/components/ResultCount.js
+++ src/components/ResultCount.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const ResultCount = ({ filteredEntries }) => {
+const ResultCount = ({ count }) => {
   return (
-    <p>{filteredEntries.length}件の結果</p>
+    <p>{count}件の結果</p>
   );
 };
 
 const mapStateToProps = state => {
-  return { filteredEntries: state.filteredEntries };
+  return { count: state.filteredEntries.length };
 };
 
 export default connect(mapStateToProps)(ResultCount);
