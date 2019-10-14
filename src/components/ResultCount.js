d32635956e93f6c9b330ee2addd52a51bea17f39
healthypackrat <healthypackrat@gmail.com>
2019-10-14 13:41:01 +0900

    Display total chars

--- src/components/ResultCount.js
+++ src/components/ResultCount.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const ResultCount = ({ count }) => {
+const ResultCount = ({ entry_count, char_count }) => {
   return (
-    <p>{count}件の結果</p>
+    <p>{entry_count}件の結果、{char_count}文字</p>
   );
 };
 
 const mapStateToProps = state => {
-  return { count: state.entries.filteredEntries.length };
+  return {
+    entry_count: state.entries.filteredEntries.length,
+    char_count: state.entries.filteredEntries.reduce((sum, entry) => sum + entry.total, 0).toLocaleString()
+  };
 };
 
 export default connect(mapStateToProps)(ResultCount);
